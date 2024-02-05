db24e31c4de83454420579d63a754ac38c84aa93
gregpec <gregpec@yahoo.com>
2024-02-05 20:51:54 +0100

    zadanie nr 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -23,28 +23,28 @@ Console.WriteLine($" Max: {statistics.Max}");
 
 var statisticsWitchForEach = employee.GetStatisticsWithForEach();
 Console.Write("\n");
-Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statistics.Average:N2}");
-Console.WriteLine($" Min: {statistics.Min}");
-Console.WriteLine($" Max: {statistics.Max}");
+Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statisticsWitchForEach.Average:N2}");
+Console.WriteLine($" Min: {statisticsWitchForEach.Min}");
+Console.WriteLine($" Max: {statisticsWitchForEach.Max}");
 
 var statisticsWitchFor = employee.GetStatisticsWithFor();
 Console.Write("\n");
-Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statistics.Average:N2}");
-Console.WriteLine($" Min: {statistics.Min}");
-Console.WriteLine($" Max: {statistics.Max}");
+Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statisticsWitchFor.Average:N2}");
+Console.WriteLine($" Min: {statisticsWitchFor.Min}");
+Console.WriteLine($" Max: {statisticsWitchFor.Max}");
 
 
 var statisticsWithDoWhile = employee.GetStatisticsWithDoWhile();
 Console.Write("\n");
-Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statistics.Average:N2}");
-Console.WriteLine($" Min: {statistics.Min}");
-Console.WriteLine($" Max: {statistics.Max}");
+Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statisticsWithDoWhile.Average:N2}");
+Console.WriteLine($" Min: {statisticsWithDoWhile.Min}");
+Console.WriteLine($" Max: {statisticsWithDoWhile.Max}");
 
 var statisticsWithWhile = employee.GetStatisticsWithWhile();
 Console.Write("\n");
-Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statistics.Average:N2}");
-Console.WriteLine($" Min: {statistics.Min}");
-Console.WriteLine($" Max: {statistics.Max}");
+Console.WriteLine("Employee named " + employee.Name + " has " + $"average: {statisticsWithWhile.Average:N2}");
+Console.WriteLine($" Min: {statisticsWithWhile.Min}");
+Console.WriteLine($" Max: {statisticsWithWhile.Max}");
 
 
 
