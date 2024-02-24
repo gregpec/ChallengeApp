da6cc94da3ba0f3459ca0a39891d6f70b339cce2
gregpec <gregpec@yahoo.com>
2024-02-24 17:45:31 +0100

    zadanie nr 18

---
+++ ChallengeApp/ChallengeApp/EmployeeInFile.cs
@@ -0,0 +1,129 @@
+﻿namespace ChallengeApp
+    {
+        public class EmployeeInFile : EmployeeBase
+        {
+            private const string fileName = @"D:\Projekty\grades.txt";
+            public EmployeeInFile(string name, string surname, char sex)
+                : base(name, surname, sex)
+            {
+            }
+            public override event GradeAddedDelegate GradeAdded;
+            public override void AddGrade(float grade)
+            {
+                if (grade >= 0 && grade <= 100)
+                {
+                    Console.Write("the value of grade is correct and is converted to float\n");
+                    using (var writer = File.AppendText(fileName))
+                        writer.WriteLine(grade);
+                    if (GradeAdded != null)
+                    {
+                        GradeAdded(this, new EventArgs());    
+                    }
+                }
+                else
+                {
+                    throw new Exception("the value is out of range");
+                }
+            }
+            public override void AddGrade(double grade)
+            {
+                Console.Write("the value of number is double and ");
+                float doubleToFloat = (float)grade;
+                this.AddGrade(doubleToFloat);
+            }
+            public override void AddGrade(int grade)
+            {
+                Console.Write("the value of number is int and ");
+                float intToFloat = (float)grade;
+                this.AddGrade(intToFloat);
+            }
+            public override void AddGrade(char grade)
+            {
+                {
+                    switch (grade)
+                    {
+                        case 'A':
+                        case 'a':
+                            this.AddGrade(100);
+
+                            break;
+                        case 'B':
+                        case 'b':
+                            this.AddGrade(80);
+
+                            break;
+                        case 'C':
+                        case 'c':
+                            this.AddGrade(60);
+
+                            break;
+                        case 'D':
+                        case 'd':
+                            this.AddGrade(40);
+
+                            break;
+                        case 'E':
+                        case 'e':
+                            this.AddGrade(20);
+                            break;
+                        default:
+                            throw new Exception("wrong letter");
+                    }
+                }
+            }
+            public override void AddGrade(long grade)
+            {
+                Console.Write("the value of number is long and ");
+                float longToFloat = (float)grade;
+                this.AddGrade(longToFloat);
+            }
+            public override void AddGrade(string grade)
+            {
+                if (float.TryParse(grade, out float stringToFloat))
+                {
+                    Console.Write("the value is string of number and ");
+                    this.AddGrade(stringToFloat);
+                }
+                else
+                {
+                    throw new Exception("the value is as string of text and is incorrect");
+                }
+            }
+            public override Statistics GetStatistics() 
+            {
+                var gradesFromFile = this.ReadGradesFromFile(); 
+                var result = this.CountStatistics(gradesFromFile); 
+                return result;
+            }
+            private List<float> ReadGradesFromFile()  
+            {
+                var grades = new List<float>();  
+                if (File.Exists($"{fileName}"))
+                {
+                    using (var reader = File.OpenText($"{fileName}")) 
+                    {
+                        var line = reader.ReadLine(); 
+                        while (line != null)
+                        {
+                            var number = float.Parse(line); 
+                            grades.Add(number); 
+                            line = reader.ReadLine(); 
+                        }
+                    }
+                }
+                return grades;
+            }
+            private Statistics CountStatistics(List<float> grades) 
+            { 
+                var statistics = new Statistics();
+                foreach (var grade in grades)
+                {
+                    statistics.AddGrade(grade);
+                }
+                return statistics;
+            }
+        }
+    }
+
+         
+
--- ChallengeApp/ChallengeApp/EmployeeInMemory.cs
+++ ChallengeApp/ChallengeApp/EmployeeInMemory.cs
@@ -1,57 +1,42 @@
-﻿using static ChallengeApp.EmployeeBase;
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
-    public class Employee:IEmployee
+    public class EmployeeInMemory : EmployeeBase
     {
-        public event GradeAddedDelegate GradeAdded;
-        
         private List<float> grades = new List<float>();
-        public Employee(string name, string surname, char sex)
-        {
-            this.Name = name;
-            this.Surname = surname;
-            this.Sex = sex;
-        }
-        public Employee(string name, string surname)
-           : this(name, surname, 'M')
+        public EmployeeInMemory(string name, string surname, char sex)
+            : base(name, surname, sex)
         {
         }
-        public Employee(string name)
-           : this(name, "no surname", 'M')
-        {
-        }
-        public Employee()
-            : this("no name", "no surname", 'M')
-        {
-        }
-        public string Name { get; private set; }
-        public string Surname { get; private set; }
-        public char Sex { get; private set; }
-
-        public void AddGrade(float grade)
+        public override event GradeAddedDelegate GradeAdded;
+      
+        public override void AddGrade(float grade)
         {
             if (grade >= 0 && grade <= 100)
             {
-                Console.Write("the value of grade is correct and is converted to float\n");
                 this.grades.Add(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs()); 
+                }
             }
             else
             {
-                throw new Exception("the value is out of range\n");
+                throw new Exception("The value is out of range");
             }
         }
-        public void AddGrade(string grade)
+        public override void AddGrade(string grade)
         {
             if (float.TryParse(grade, out float stringToFloat))
             {
-                Console.Write("the value is string of number and ");
+                Console.Write("the value is string of number and: ");
                 this.AddGrade(stringToFloat);
             }
             else
-                Console.WriteLine("the value is as string of text and is incorrect");
+            {
+                throw new Exception("The value is as string of text and is incorrect");
+            }  
         }
-        public void AddGrade(char grade)
+        public override void AddGrade(char grade)
         {
             switch (grade)
             {
@@ -80,46 +65,38 @@ namespace ChallengeApp
                     this.AddGrade(20);
                     break;
                 default:
-                    throw new Exception("wrong letter");
+                    throw new Exception("Wrong letter");
             }
         }
-        public void AddGrade(double grade)
+        public override void AddGrade(double grade)
         {
-            Console.Write("the value of number is double and ");
+            Console.Write("the value of number is double and: ");
             float doubleToFloat = (float)grade;
             this.AddGrade(doubleToFloat);
         }
-        public void AddGrade(long grade)
+        public override void AddGrade(long grade)
         {
-            Console.Write("the value of number is long and ");
+            Console.Write("the value of number is long and: ");
             float longToFloat = (float)grade;
             this.AddGrade(longToFloat);
         }
-        public void AddGrade(int grade)
+        public override void AddGrade(int grade)
         {
-            Console.Write("the value of number is int and ");
+            Console.Write("the value of number is int and: ");
             float intToFloat = (float)grade;
             this.AddGrade(intToFloat);
         }
-        public Statistics GetStatistics()
+        public override Statistics GetStatistics()
         {
             var statistics = new Statistics();
-         
             foreach (var grade in this.grades)
             {
                 statistics.AddGrade(grade);
             }
             return statistics;
-
-
         }
     }
 }
-
-
-
-
-
 
 
 
